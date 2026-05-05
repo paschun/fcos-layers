c2bf9143a753e41969701ea98460904a949a597a
paschun <152430265+paschun@users.noreply.github.com>
2026-05-04 21:44:40 -0700

    Add FBS devicetree patch to custom-kernel. Add efivar + ngrep to base.

--- custom-kernel/deploy.sh
+++ custom-kernel/deploy.sh
@@ -5,6 +5,7 @@ set -xeuo pipefail
 podman build --pull=newer -t localhost/custom-kernel .
 time podman save --format oci-archive -o custom-kernel.tar localhost/custom-kernel
 rsync -vhicP ./custom-kernel.tar "$HOST":/var/tmp/
+rm -v ./custom-kernel.tar
 ssh -t "$HOST" 'sudo ostree admin pin 0'
 ssh -t "$HOST" 'sudo rpm-ostree rebase ostree-unverified-image:oci-archive:/var/tmp/custom-kernel.tar'
 # then reboot
