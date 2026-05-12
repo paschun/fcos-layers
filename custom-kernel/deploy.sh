24d174a185c06d2fdd7e79248087c2f63e89b6f8
paschun <152430265+paschun@users.noreply.github.com>
2026-05-11 17:28:03 -0700

    Get specific bcachefs-tools .rpm file incase there are multiple.

--- custom-kernel/deploy.sh
+++ custom-kernel/deploy.sh
@@ -6,8 +6,8 @@ podman build --pull=newer -t localhost/custom-kernel .
 time podman save --format oci-archive -o custom-kernel.tar localhost/custom-kernel
 rsync -vhicP ./custom-kernel.tar "$HOST":/var/tmp/
 rm -v ./custom-kernel.tar
-ssh -t "$HOST" 'sudo ostree admin pin 0'
-ssh -t "$HOST" 'sudo rpm-ostree rebase ostree-unverified-image:oci-archive:/var/tmp/custom-kernel.tar'
+ssh "$HOST" 'sudo ostree admin pin 0'
+ssh "$HOST" 'sudo rpm-ostree rebase ostree-unverified-image:oci-archive:/var/tmp/custom-kernel.tar'
 # then reboot
 #
 # to delete:
